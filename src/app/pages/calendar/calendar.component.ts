5c406e1fb6ccb0ce2cc5bf56270298d0384c6ac4
Koushik Chatterjee <koushik_chatterjee@epam.com>
2025-02-07 11:12:59 +0530

    🚀 RELEASE: Ready for release

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PageheaderComponent } from '../pageheader/pageheader.component';
+import { LearningPointComponent } from '../learning-point/learning-point.component';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
-  templateUrl: './calendar.component.html'
+  imports: [FormsModule, PageheaderComponent, LearningPointComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './calendar.component.html',
 })
 export class CalendarComponent {
+  lp = [
+    'How to handle calendar elements',
+    'How to set dynamic dates',
+    'Java calendar & moment.js',
+  ];
+  link = '/video/Calender';
+  birthday: string = '';
 
+  onSubmit() {
+    console.log('Selected Birthday:', this.birthday);
+  }
 }
