de27b1d699fcad38d5c13230fa6aff99fdff54a4
ortoniKC <koushik350@gmail.com>
2025-02-16 23:07:45 +0530

    🚀 RELEASE: Hosted in netlify

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,6 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
-import { EnvironmentProviders, makeEnvironmentProviders } from '@angular/core';
-
 import { routes } from './app.routes';
 import {
   provideClientHydration,
@@ -13,14 +11,12 @@ import {
 } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
-import { JokeService } from './service/jokes.service';
 import { AdsenseModule } from 'ng2-adsense';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MarkdownModule } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideEnvironment(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
@@ -40,24 +36,3 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
   ],
 };
-
-function provideEnvironment(): EnvironmentProviders {
-  return makeEnvironmentProviders([
-    {
-      provide: 'ENV',
-      useValue: {
-        production: true,
-        firebaseConfig: {
-          apiKey: 'AIzaSyA6GNjn5DzwmkfYISu2cD84HBfkstrlb7s',
-          authDomain: 'play-letcode.firebaseapp.com',
-          databaseURL: 'https://play-letcode.firebaseio.com',
-          projectId: 'play-letcode',
-          storageBucket: 'play-letcode.appspot.com',
-          messagingSenderId: '329152186575',
-          appId: '1:329152186575:web:ced65caa5001e68d808fba',
-          measurementId: 'G-STHL8RV4RK',
-        },
-      },
-    },
-  ]);
-}
