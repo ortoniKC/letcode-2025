597c299f31c7ff1563c3ec13c84a74d6b432ca58
Koushik Chatterjee <koushik350@gmail.com>
2025-05-21 14:51:09 +0530

    Update repos.component.ts

--- src/app/github/repos/repos.component.ts
+++ src/app/github/repos/repos.component.ts
@@ -11,13 +11,16 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-repos',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './repos.component.html',
 })
 export class ReposComponent implements OnInit, OnChanges {
-  @Input()
-  repoUrl!: string;
-  repos: any = [];
+  @Input() repoUrl!: string;
+  repos: any[] = [];
+  paginatedRepos: any[] = [];
+  currentPage = 1;
+  itemsPerPage = 5;
 
   constructor(
     private gitService: GithubService,
@@ -26,15 +29,26 @@ export class ReposComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     if (this.repoUrl) {
-      this.gitService.getRepos(this.repoUrl).subscribe(
-        (repo) => {
-          this.repos = repo;
-          this.changeRef.detectChanges();
-        },
-        (err) => {}
-      );
+      this.gitService.getRepos(this.repoUrl).subscribe((repo) => {
+        this.repos = repo;
+        this.setPage(1);
+        this.changeRef.detectChanges();
+      });
     }
   }
 
   ngOnInit(): void {}
+
+  setPage(page: number): void {
+    this.currentPage = page;
+    const start = (page - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.paginatedRepos = this.repos.slice(start, end);
+  }
+
+  get totalPages(): number[] {
+    return Array(Math.ceil(this.repos.length / this.itemsPerPage))
+      .fill(0)
+      .map((_, i) => i + 1);
+  }
 }
