dd01b89ddd766d2daf13a528e340de8372ccf56a
Koushik Chatterjee <koushik350@gmail.com>
2025-05-23 02:18:40 +0530

    Update github.service.ts

--- src/app/service/github.service.ts
+++ src/app/service/github.service.ts
@@ -1,17 +1,16 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class GithubService {
   constructor(private http: HttpClient) {}
 
-  getUserDetail(user: string) {
-    return this.http.get(`https://api.github.com/users/${user}`);
-  }
+  getRepos(url: string): Observable<any[]> {
+    return this.http.get<any[]>(url); 
+}
 
-  getRepos(repoURL: string) {
-    return this.http.get(repoURL);
+  getUserDetail(username: string): Observable<any> {
+    return this.http.get<any>(`https://api.github.com/users/${username}`);
   }
 }
